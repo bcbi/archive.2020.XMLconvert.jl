657068c60f7bb013ac98697665c3e82fd5020b8e
Isa Restrepo <isabelrestre@gmail.com>
2016-05-13 17:03:58 -0400

    Renamed xml2dict.jl to xml_to_dict.jl

--- src/XMLconvert.jl
+++ src/XMLconvert.jl
@@ -6,7 +6,7 @@ using DataStructures
 export xml2json, parse_file, root, xml2dict, show_key_structure
 
 include("xml_to_json.jl")
-include("xml2dict.jl")
+include("xml_to_dict.jl")
 include("utils.jl")
 
 end # module
