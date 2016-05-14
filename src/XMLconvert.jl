3767ee8c2da56201ccc52beadc0855ef170dabe6
paulstey <paulstey@gmail.com>
2016-05-14 09:04:06 -0400

    exporting parse_string

--- src/XMLconvert.jl
+++ src/XMLconvert.jl
@@ -3,7 +3,7 @@ module XMLconvert
 using LightXML
 using DataStructures
 
-export xml2json, parse_file, root, xml2dict, show_key_structure
+export xml2json, parse_file, parse_string, root, xml2dict, show_key_structure
 
 include("xml_to_json.jl")
 include("xml_to_dict.jl")
