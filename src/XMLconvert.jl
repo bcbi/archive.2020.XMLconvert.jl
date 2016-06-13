495793bdfa44157d6628cdff51026beb63075452
paulstey <paulstey@gmail.com>
2016-06-13 13:49:41 -0400

    added attributes_to_elements() function

--- src/XMLconvert.jl
+++ src/XMLconvert.jl
@@ -4,7 +4,7 @@ using LightXML
 using DataStructures
 
 export xml2json, xml2dict, parse_file, parse_string,
-       root, show_key_structure, flatten
+       root, show_key_structure, flatten, attributes_to_elements!
 
 include("xml_to_json.jl")
 include("xml_to_dict.jl")
