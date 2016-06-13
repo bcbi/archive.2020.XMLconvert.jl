c08b10d981b7bff851bac47bdf2f121964910153
paulstey <paulstey@gmail.com>
2016-06-13 13:48:08 -0400

    added attributes_to_elements() function

--- src/xml_to_dict.jl
+++ src/xml_to_dict.jl
@@ -41,3 +41,36 @@ function xml2dict(element_node, replace_newline = nothing)
         return content(element_node)
     end
 end
+
+
+
+
+# This function converts *some* attributes to elements in
+# a parsed XML doc. In particular, it will only convert those
+# attributes that are in self-closing tags. Those attributes
+# that are part of a node whose content is not equal to "" will
+# be ignored by this function. Note that the function modifies
+# the parsed XML document in place.
+
+function attributes_to_elements!(element_node)
+    if has_any_children(element_node)
+        ces = collect(child_elements(element_node))
+        for c in ces
+            attributes_to_elements!(c)
+        end
+    elseif content(element_node) == ""
+        if has_attributes(element_node)
+            attr_dict_array = Array{Dict}(0)
+            push!(attr_dict_array, attributes_dict(element_node))
+            for i = 1:length(attr_dict_array)
+                for (k, v) in zip(keys(attr_dict_array[i]), values(attr_dict_array[i]))
+                    elem = new_child(element_node, k)
+                    add_text(elem, v)
+                end
+            end
+        end
+    end
+end
+
+
+
