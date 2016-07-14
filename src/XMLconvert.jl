309bdeb83d0e41cb46c6eb98fc00a8cc30ce4c20
Isa Restrepo <isabelrestre@gmail.com>
2016-07-14 10:45:01 -0400

    including xml_to_file.jl to the module

--- src/XMLconvert.jl
+++ src/XMLconvert.jl
@@ -9,6 +9,7 @@ export xml2json, xml2dict, xmlASCII2file, parse_file, parse_string,
 
 include("xml_to_json.jl")
 include("xml_to_dict.jl")
+include("xml_to_file.jl")
 include("utils.jl")
 include("flatten_multidict.jl")
 
