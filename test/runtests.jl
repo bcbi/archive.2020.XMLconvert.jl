c104bb87e1c91219852d737371ebb3cbccd7471b
paulstey <paulstey@gmail.com>
2016-07-18 10:19:59 -0400

    added some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,48 @@
 using XMLconvert
 using Base.Test
 
-fname = "ex1.xml"
+file1 = "ex1.xml"
+file2 = "ex2.xml"
+xdoc = parse_file(file1)
+xroot = root(xdoc)
 
-function load_and_parse(filename)
-    xdoc = parse_file(filename)
-    xroot = root(xdoc)
-    mdict = xml2dict(xroot)
+xdoc2 = parse_file(file2)
+xroot2 = root(xdoc2)
+
+
+function xml2dict_check(root_node)
+    mdict = xml2dict(root_node)
+    return isa(mdict, DataStructures.MultiDict)
+end
+
+
+function json_conversion_check(root_node)
+    json_str = xml2json(root_node)
+    return isa(json_str, ASCIIString)
+end
+
+
+function attributes_to_elements_check(root_node)
+    attributes_to_elements!(root_node)
+    mdict = xml2dict(root_node)
     return mdict
 end
 
-chk = load_and_parse(fname)
+@test xml2dict_check(xroot)
+@test json_conversion_check(xroot)
 
-@test isa(chk, DataStructures.MultiDict)
+muldict = attributes_to_elements_check(xroot2)
+
+@test isa(muldict, DataStructures.MultiDict)
+
+@test muldict["owner"][1]["age"][1] == 99
+@test muldict["owner"][1]["name"][1] == "Henry Chinaski"
+@test muldict["book"][1]["title"][1]["title"][1] == "Everyday Italian"
+
+fdict = flatten(muldict)
+
+@test haskey(fdict, "book-author")
+@test haskey(fdict, "book-title-lang")
+@test length(fict["book-author"]) == 3
+
+@test isa(show_key_structure(muldict), Void)
